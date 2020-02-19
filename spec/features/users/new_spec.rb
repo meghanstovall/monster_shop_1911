08af3925905088f4e9afbb21ce48d46468cc3178
Alex Gallant <agallant1201@gmail.com>
2020-02-18 17:55:41 -0700

    Test to make sure email is unique and has repopulated fields for information that was correctly entered
    Co-Authored-By: Elom A. <eamouzou@users.noreply.github.com>
    Co-Authored-By: Meghan Stovall <meghanstovall@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -59,10 +59,51 @@ describe 'new user' do
       expect(current_path).to eq("/register")
       expect(page).to have_content("Zip can't be blank")
     end
-  end
 
-  # As a visitor
-  # When I visit the user registration page
-  # And I do not fill in this form completely,
-  # I am returned to the registration page
-  # And I see a flash message indicating that I am missing required fields
+    it "needs a unique email to register" do
+
+      visit '/'
+
+      click_link 'Register'
+
+      expect(current_path).to eq("/register")
+
+      user_2 = User.create!(
+        name: "Peter Webber",
+        street_address: "30 Girls Street",
+        city: "Los Angeles",
+        state: "CA",
+        zip: 90036,
+        email: "pilotpete@gmail.com",
+        password: "password1",
+        password_confirmation: "password1"
+      )
+
+      name = "Ryan Allen"
+      street_address = "45433 fake st."
+      city = "Denver"
+      state = "Colorado"
+      zip = 80234
+      email = "pilotpete@gmail.com"
+      password = "BestFakerEver"
+      password_confirmation = "BestFakerEver"
+
+      fill_in :name, with: name
+      fill_in :street_address, with: street_address
+      fill_in :city, with: city
+      fill_in :state, with: state
+      fill_in :zip, with: zip
+      fill_in :email, with: email
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: password_confirmation
+
+      click_on 'Create User'
+
+      expect(current_path).to eq('/register')
+      expect(page).to have_content("Email has already been taken")
+      #Above expectation is created with the uniqueness validation
+      expect(page).not_to have_content(email)
+      expect(find_field("name").value).to eq(name)
+      expect(find_field("city").value).to eq(city)
+    end
+  end
