2b90dc0e18561895a99abbf231584bf0ee8f2b1a
Alex Gallant <agallant1201@gmail.com>
2020-02-19 15:11:33 -0700

    Add current user helper method in applications_controller
    Co-Authored-By: Elom A. <eamouzou@users.noreply.github.com>
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
   protect_from_forgery with: :exception
 
-  helper_method :cart
+  helper_method :cart, :current_user
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
+  end
+
+  def current_user
+    # require "pry"; binding.pry
+    return false unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
   end
 
 end
