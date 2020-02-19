3ea4166d690ef7157bdfd3366ecb92d0a4136ebd
Alex Gallant <agallant1201@gmail.com>
2020-02-19 15:11:13 -0700

    Add session creation for new user
    Co-Authored-By: Elom A. <eamouzou@users.noreply.github.com>
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     # @user = User.new(user_params)
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.name} is now logged in"
       redirect_to '/profile'
     else
