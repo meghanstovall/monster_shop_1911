d26a28d0602a7b0b011c72a9f9fe8e88d09e8cd6
Elom Amouzou <elom1990@yahoo.com>
2020-02-20 12:12:33 -0700

    add login test for good and bad credentials

---
+++ spec/features/users/login_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe "different users login" do
+  before(:each) do
+    @visitor_user = User.create!(name: "Damon",street_address: "2417 E. Curtis St. Denver, CO",
+                              city: "denver",state: "CO",zip: "80206",email: "new_email0@gmail.com",password: "hamburger0", role: 0)
+    @regular_user = User.create!(name: "Mike",street_address: "456 Logan St. Denver, CO",
+                              city: "denver",state: "CO",zip: "80206",email: "new_email1@gmail.com",password: "hamburger1", role: 1)
+    @merchant_user = User.create!(name: "Ben",street_address: "891 Penn St. Denver, CO",
+                              city: "denver",state: "CO",zip: "80206",email: "new_email2@gmail.com",password: "hamburger2", role: 2)
+    @admin_user = User.create!(name: "John",street_address: "123 Colfax St. Denver, CO",
+                              city: "denver",state: "CO",zip: "80206",email: "new_email3@gmail.com",password: "hamburger3", role: 3)
+    visit '/'
+  end
+
+  describe 'user visits login path' do
+    it 'logs in as a regular user' do
+      click_link 'Login'
+      expect(current_path).to eq('/login')
+
+      fill_in :email, with: @regular_user.email
+      fill_in :password, with: 'hamburger1'
+      click_button 'Log In'
+
+      expect(current_path).to eq('/profile')
+      expect(page).to have_content(@regular_user.name)
+      expect(page).to have_content(@regular_user.street_address)
+      expect(page).to have_content(@regular_user.city)
+      expect(page).to have_content(@regular_user.state)
+      expect(page).to have_content(@regular_user.zip)
+      expect(page).to have_content(@regular_user.email)
+      expect(page).to have_content("#{@regular_user.name} is logged in.")
+    end
+
+    it 'logs in as a merchant user' do
+      click_link 'Login'
+      expect(current_path).to eq('/login')
+
+      fill_in :email, with: @merchant_user.email
+      fill_in :password, with: 'hamburger2'
+      click_button 'Log In'
+
+      expect(current_path).to eq('/merchant/dashboard')
+    end
+
+    it 'logs in as an admin user' do
+      click_link 'Login'
+      expect(current_path).to eq('/login')
+
+      fill_in :email, with: @admin_user.email
+      fill_in :password, with: 'hamburger3'
+      click_button 'Log In'
+
+      expect(current_path).to eq('/admin/dashboard')
+    end
+  end
+
+  it "cannot log in with incorrect credentials" do
+    visit '/login'
+    fill_in :email, with: @regular_user.email
+    fill_in :password, with: "bonsai"
+    click_button "Log In"
+    expect(current_path).to eq("/login")
+    expect(page).to have_content("Login Failed; Your Credentials were Incorrect")
+  end
+end
