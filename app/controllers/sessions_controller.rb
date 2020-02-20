a822cbf620361f78681bd31e664e10c4f705a620
Elom Amouzou <elom1990@yahoo.com>
2020-02-19 18:59:00 -0700

    add conditionals to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,21 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    # require "pry"; binding.pry
     if user != nil && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
     end
-    if user.merchant?
+
+    if user.default?
+      redirect_to '/profile'
+      flash[:success] = "#{user.name} is logged in!"
+    elsif user.merchant?
       redirect_to '/merchant/dashboard'
+      flash[:success] = "#{user.name} is logged in!"
+    elsif user.admin?
+      redirect_to '/admin/dashboard'
+      flash[:success] = "#{user.name} is logged in!"
     end
   end
+
 end
