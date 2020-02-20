d7076c8b2b115a1d83f54e5c6b3416ff438f6ebd
Alex Gallant <agallant1201@gmail.com>
2020-02-19 18:57:44 -0700

    Delete pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    # require "pry"; binding.pry
     if user != nil && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
