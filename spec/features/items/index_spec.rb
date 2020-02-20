660123c5b91a61e4a8f6cde033f4bb7e9986c6cc
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-20 16:53:50 -0700

    Add test to make sure all items are on the index page and you can click the item's image
    Co-Authored-By: Alex Gallant <agallant1201@gmail.com>
    Co-Authored-By: Elom A. <eamouzou@users.noreply.github.com>
    Co-Authored-By: Pierce <pbalworth@gmail.com>

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Items Index Page" do
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
     end
@@ -57,5 +56,46 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_css("img[src*='#{@dog_bone.image}']")
       end
     end
+
+    it "can see all items when visiting the show page" do
+
+      visit "/items"
+
+      within "#item-#{@tire.id}" do
+        expect(page).to have_content(@tire.name)
+        expect(page).to have_css("img[src*='#{@tire.image}']")
+
+        find(:xpath, "//a/img[@alt='#{@tire.name}']/..").click
+        expect(current_path).to eq("/items/#{@tire.id}")
+      end
+
+      visit "/items"
+
+      within "#item-#{@pull_toy.id}" do
+        expect(page).to have_content(@pull_toy.name)
+        expect(page).to have_css("img[src*='#{@pull_toy.image}']")
+
+        find(:xpath, "//a/img[@alt='#{@pull_toy.name}']/..").click
+        expect(current_path).to eq("/items/#{@pull_toy.id}")
+      end
+
+      visit "/items"
+
+      within "#item-#{@dog_bone.id}" do
+        expect(page).to have_content(@dog_bone.name)
+        expect(page).to have_css("img[src*='#{@dog_bone.image}']")
+
+        find(:xpath, "//a/img[@alt='#{@dog_bone.name}']/..").click
+        expect(current_path).to eq("/items/#{@dog_bone.id}")
+      end
+    end
   end
 end
+
+# User Story 17, Items Index Page
+#
+# As any kind of user on the system
+# I can visit the items catalog ("/items")
+# I see all items in the system except disabled items
+#
+# The item image is a link to that item's show page
