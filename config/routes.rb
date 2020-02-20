5f2182762bef445ab8997784ed4d76f73ea39ec2
Elom Amouzou <elom1990@yahoo.com>
2020-02-19 18:58:32 -0700

    add admin dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -38,9 +38,11 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
-  get "/profile", to: 'users#show'
+  get '/profile', to: 'users#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
   get '/merchant/dashboard', to: 'dashboards#show'
+
+  get '/admin/dashboard', to: 'dashboards#show'
 end
