0339e2b881d5bc518eda23cf15699f3e295ed604
Elom Amouzou <elom1990@yahoo.com>
2020-02-19 18:58:19 -0700

    delete user stories in test

--- spec/features/navigation/navigation_spec.rb
+++ spec/features/navigation/navigation_spec.rb
@@ -77,24 +77,3 @@ RSpec.describe 'Site Navigation' do
     expect(page).to_not have_link("Register")
   end
 end
-
-# User Story 4, Merchant Navigation
-#
-# As a merchant employee
-# I see the same links as a regular user
-# Plus the following links:
-# - a link to my merchant dashboard ("/merchant")
-
-
-# User Story 3, User Navigation
-#
-# As a default user
-# I see the same links as a visitor
-# Plus the following links
-# - a link to my profile page ("/profile")
-# - a link to log out ("/logout")
-#
-# Minus the following links
-# - I do not see a link to log in or register
-#
-# I also see text that says "Logged in as Mike Dao" (or whatever my name is)
