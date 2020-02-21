1be2634774ceab4172892f24bac4691134b9a393
Pierce Alworth <pbalworth@gmail.com>
2020-02-20 18:08:32 -0700

    Add routes get and patch for profile edit and patch.
    Co-Authored-By: null <agallant121@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ Rails.application.routes.draw do
   post '/register', to: 'users#create'
 
   get "/profile", to: 'users#show'
+  get "/profile/:profile_id/edit", to: "users#edit"
+  patch '/profile', to: 'users#update'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/', to: 'sessions#destroy'
