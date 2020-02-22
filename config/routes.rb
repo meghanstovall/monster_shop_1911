46097dcaa794904acfa353c1d4813a22b41652c8
Elom Amouzou <elom1990@yahoo.com>
2020-02-22 16:48:03 -0700

    add route for orders update
    Co-Authored-By: Meghan Stovall <meghanstovall15@augustana.edu>
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>
    Co-Authored-By: Alex Gallant <agallant1201@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
+  patch "/orders/:id", to: "orders#update"
 
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
