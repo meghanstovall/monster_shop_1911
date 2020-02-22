d9af84d382ce30978b85733b3042b9f4ae67191b
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-22 13:17:32 -0700

    Add a default status as pending for Orders, as well as added total_item_quantity method to the order model
    Co-Authored-By: Elom A <54334799+eamouzou@users.noreply.github.com>
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>
    Co-Authored-By: Alex Gallant <agallant1201@gmail.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,20 @@
-class Order <ApplicationRecord
+class Order < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
 
+  belongs_to :user
   has_many :item_orders
   has_many :items, through: :item_orders
+  before_save :default_values
+
+  def default_values
+    self.status = 'pending'
+  end
 
   def grandtotal
     item_orders.sum('price * quantity')
+  end
+
+  def total_item_quantity
+    item_orders.sum('quantity')
   end
 end
