b6439a45d5b1fa581941a0635774408583574648
Elom Amouzou <elom1990@yahoo.com>
2020-02-23 13:16:57 -0700

    add orders to show action and add update action using ship_and_fulfill
    Co-Authored-By: Meghan Stovall <meghanstovall15@augustana.edu>
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>
    Co-Authored-By: Alex Gallant <agallant1201@gmail.com>

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class Admin::DashboardController < Admin::BaseController
 
   def show
+    @orders = Order.all
+  end
 
+  def update
+    order = Order.find(params[:id])
+    order.ship_and_fulfill
+    redirect_to "/admin/dashboard"
   end
 end
