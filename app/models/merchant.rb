8a5bea6c56aa2169ecf05f66a6bcda218ade25ea
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-23 14:58:24 -0700

    Add disabled to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,6 +9,7 @@ class Merchant <ApplicationRecord
                         :state,
                         :zip
 
+  validates_inclusion_of :disabled, :in => [true, false]
 
   def no_orders?
     item_orders.empty?
