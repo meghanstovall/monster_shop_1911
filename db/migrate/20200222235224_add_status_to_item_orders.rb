0728a2dc491dda426197b8fd771d6860288980b0
Elom Amouzou <elom1990@yahoo.com>
2020-02-22 17:10:03 -0700

    update migration to integer
    Co-Authored-By: Meghan Stovall <meghanstovall15@augustana.edu>
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>
    Co-Authored-By: Alex Gallant <agallant1201@gmail.com>

--- db/migrate/20200222235224_add_status_to_item_orders.rb
+++ db/migrate/20200222235224_add_status_to_item_orders.rb
@@ -1,5 +1,6 @@
 class AddStatusToItemOrders < ActiveRecord::Migration[5.1]
   def change
-    add_column :item_orders, :status, :string, default: "unfulfilled"
+    add_column :item_orders, :status, :integer, default: 0
+    ItemOrder.all.where(status: nil).update_all(status: 0)
   end
 end
