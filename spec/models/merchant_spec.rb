d5ea11bfb5f3d2b03a0263df13e523e22f2e0f06
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-23 14:58:08 -0700

    Test that a merchant has disabled attribute

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,7 +7,7 @@ describe Merchant, type: :model do
     it { should validate_presence_of :city }
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
-    it { should validate_presence_of :disabled }
+    it { should validate_inclusion_of(:disabled).in_array([true,false]) }
   end
 
   describe "relationships" do
@@ -17,9 +17,10 @@ describe Merchant, type: :model do
 
   describe 'instance methods' do
     before(:each) do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, disabled: false)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
     end
+
     it 'no_orders' do
       expect(@meg.no_orders?).to eq(true)
 
