c3b0d2adfb160098bca5d006944a434b7a294ded
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-23 14:38:32 -0700

    Add test to check that an admin can disable a merchant

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -24,4 +24,31 @@ RSpec.describe 'merchant index page', type: :feature do
       expect(current_path).to eq("/merchants/new")
     end
   end
+
+  describe "as a merchant" do
+    before :each do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      @dog_shop = Merchant.create(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
+
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    end
+
+    it "as an admin, can disable a merchant" do
+      visit "/admin/merchants"
+
+      within "#merchant-#{@bike_shop.id}" do
+        click_button "Disable"
+      end
+
+      expect(current_path).to eq("/admin/merchants")
+      expect(@bike_shop.disabled).to eq("true")
+    end
+    # As an admin
+    # When I visit the admin's merchant index page ('/admin/merchants')
+    # I see a "disable" button next to any merchants who are not yet disabled
+    # When I click on the "disable" button
+    # I am returned to the admin's merchant index page where I see that the merchant's account is now disabled
+    # And I see a flash message that the merchant's account is now disabled
+  end
 end
