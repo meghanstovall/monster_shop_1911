89ac1b058ffa679cde7c4caa1d850780ce4dbf82
Alex Gallant <agallant1201@gmail.com>
2020-02-22 19:34:16 -0700

    Change default role
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20200222231150) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 1
+    t.integer "role", default: 0
     t.bigint "merchant_id"
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
