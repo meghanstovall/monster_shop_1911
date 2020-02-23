79d8b508e7a9e62d190e5a1a224a30130f642ab4
Alex Gallant <agallant1201@gmail.com>
2020-02-22 19:34:40 -0700

    Trying to get the orders
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Merchant::DashboardController < Merchant::BaseController
   def show
-
+    require "pry"; binding.pry
+    merchant = Merchant.find(current_user.merchant.id)
+    @orders = merchant.orders.all.distinct
   end
 end
