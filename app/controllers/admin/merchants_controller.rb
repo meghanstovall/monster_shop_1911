50f198bf1604c4c54c30725e807b5fc399593776
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-24 10:51:46 -0700

    Called model method to deactivate merchant's items when merchant is disabled

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,6 +6,9 @@ class Admin::MerchantsController < Admin::BaseController
   def update
     merchant = Merchant.find(params[:id])
     merchant.enable_disable
+    if merchant.disabled
+      merchant.deactivate_items
+    end
     flash[:notice] = "Merchant has been disabled"
 
     redirect_to '/admin/merchants'
