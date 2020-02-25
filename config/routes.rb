cf8572541a3e53e6b855b5b6c65bd707195cfa29
Alex Gallant <agallant1201@gmail.com>
2020-02-25 08:29:24 -0700

    Edit spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
-  
+
   resources :merchants
 
   resources :items, only: [:index, :show, :edit, :update]
