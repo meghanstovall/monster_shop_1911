8d43415e5dc0ce8b448e05bfae3e17e16c371956
Elom Amouzou <elom1990@yahoo.com>
2020-02-25 11:02:23 -0700

    refactor controller to slim it up

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,8 +10,7 @@ class Admin::ItemsController < Admin::BaseController
   def destroy
     item = Item.find(params[:item_id])
     item.destroy
-    flash[:destroy] = "#{item.name} is now deleted."
-    redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
+    destroy_flash_and_redirect(item)
   end
 
   def new
@@ -35,40 +34,50 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def default_image(item)
-    if item.image == '' || item.image == nil
-      item.image = "https://i.picsum.photos/id/866/200/300.jpg"
-    end
+    item.image = "https://i.picsum.photos/id/866/200/300.jpg" if item.image == '' || item.image == nil
   end
 
   def activation_process(item)
     item.updates_active
-    if item.active? == false
-      flash[:update] = "#{item.name} is no longer for sale."
-    else
-      flash[:update] = "#{item.name} is now available for sale."
-    end
+    activation_process_flashes(item)
     redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
+  end
+
+  def activation_process_flashes(item)
+    flash[:update] = "#{item.name} is no longer for sale." if item.active? == false
+    flash[:update] = "#{item.name} is now available for sale." if item.active? == true
   end
 
   def save_process(item, render_arg, message)
     default_image(item)
-    if item.save
-      flash[:saved] = message
-      redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
-    else
-      flash[:error] = item.errors.full_messages.to_sentence
-      render render_arg
-    end
+    save_process_step_1(message) if item.save == true
+    save_process_step_2(item, render_arg) if item.save == false
+  end
+
+  def save_process_step_1(message)
+    flash[:saved] = message
+    redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
+  end
+
+  def save_process_step_2(item, render_arg)
+    flash[:error] = item.errors.full_messages.to_sentence
+    render render_arg
   end
 
   def update_item
     @item = Item.find(params[:item_id])
-    if params[:commit] == "Update Item"
-      @item.update(item_params)
-      save_process(@item, render_arg = :edit, message = "#{@item.name} is updated.")
-    else
-      activation_process(@item)
-    end
+    update_item_step_1(item = @item) if params[:commit] == "Update Item"
+    activation_process(@item) if params[:commit] != "Update Item"
+  end
+
+  def update_item_step_1(item)
+    item.update(item_params)
+    save_process(item, render_arg = :edit, message = "#{item.name} is updated.")
+  end
+
+  def destroy_flash_and_redirect(item)
+    flash[:destroy] = "#{item.name} is now deleted."
+    redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
   end
 
 end
