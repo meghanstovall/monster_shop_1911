f3f8edf86da3acfeb685b7852960686cff05b4dd
Elom Amouzou <elom1990@yahoo.com>
2020-02-24 21:03:59 -0700

    add new and create actions with params and default image

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,4 +21,33 @@ class Admin::ItemsController < Admin::BaseController
     redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
   end
 
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    item = @merchant.items.create(item_params)
+    default_image(item)
+    if item.save
+      flash[:saved] = "#{item.name} is saved."
+      redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
+    else
+      flash[:error] = item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name,:description,:price,:inventory,:image)
+  end
+
+  def default_image(item)
+    if item.image == '' || item.image == nil
+      item.image = "https://i.picsum.photos/id/866/200/300.jpg"
+    end
+  end
+
 end
