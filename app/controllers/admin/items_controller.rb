b1a047f10dc5cbf701b0143cb44fc79f21e3e99a
Elom Amouzou <elom1990@yahoo.com>
2020-02-24 21:45:46 -0700

    revise update action for different functionality, add helper methods

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,14 +4,13 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def update
-    item = Item.find(params[:item_id])
-    item.updates_active
-    if item.active? == false
-      flash[:update] = "#{item.name} is no longer for sale."
+    @item = Item.find(params[:item_id])
+    if params[:commit] == "Update Item"
+      @item.update(item_params)
+      save_process(@item, render_arg = :edit, message = "#{@item.name} is updated.")
     else
-      flash[:update] = "#{item.name} is now available for sale."
+      activation_process(@item)
     end
-    redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
   end
 
   def destroy
@@ -28,14 +27,11 @@ class Admin::ItemsController < Admin::BaseController
   def create
     @merchant = Merchant.find(params[:merchant_id])
     item = @merchant.items.create(item_params)
-    default_image(item)
-    if item.save
-      flash[:saved] = "#{item.name} is saved."
-      redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
-    else
-      flash[:error] = item.errors.full_messages.to_sentence
-      render :new
-    end
+    save_process(item, render_arg = :new, message = "#{item.name} is saved.")
+  end
+
+  def edit
+    @item = Item.find(params[:item_id])
   end
 
   private
@@ -47,6 +43,27 @@ class Admin::ItemsController < Admin::BaseController
   def default_image(item)
     if item.image == '' || item.image == nil
       item.image = "https://i.picsum.photos/id/866/200/300.jpg"
+    end
+  end
+
+  def activation_process(item)
+    item.updates_active
+    if item.active? == false
+      flash[:update] = "#{item.name} is no longer for sale."
+    else
+      flash[:update] = "#{item.name} is now available for sale."
+    end
+    redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
+  end
+
+  def save_process(item, render_arg, message)
+    default_image(item)
+    if item.save
+      flash[:saved] = message
+      redirect_to "/admin/merchants/#{params[:merchant_id]}/items"
+    else
+      flash[:error] = item.errors.full_messages.to_sentence
+      render render_arg
     end
   end
 
