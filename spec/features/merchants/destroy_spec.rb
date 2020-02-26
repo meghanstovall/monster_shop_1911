5e70ebfddb2c4f722566bdca45d41a1ac7142626
Alex Gallant <agallant1201@gmail.com>
2020-02-26 11:32:03 -0700

    Had to sign in as correct user to perform functionality
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>
    Co-Authored-By: Meghan Stovall <meghanstovall@users.noreply.github.com>
    Co-Authored-By: Elom A. <eamouzou@users.noreply.github.com>

--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -4,8 +4,17 @@ RSpec.describe "As a visitor" do
   describe "When I visit a merchant show page" do
     it "I can delete a merchant" do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      admin_user = User.create!(name: "Ben", street_address: "891 Penn St. Denver, CO",
+                                city: "denver",state: "CO",zip: "80206",email: "merchant@gmail.com",password: "hamburger2", role: 3)
+      visit '/'
+      click_link 'Login'
+      expect(current_path).to eq('/login')
 
-      visit "merchants/#{bike_shop.id}"
+      fill_in :email, with: admin_user.email
+      fill_in :password, with: 'hamburger2'
+      click_button 'Log In'
+
+      visit "/merchants/#{bike_shop.id}"
 
       click_on "Delete Merchant"
 
@@ -16,8 +25,17 @@ RSpec.describe "As a visitor" do
     it "I can delete a merchant that has items" do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      admin_user = User.create!(name: "Ben", street_address: "891 Penn St. Denver, CO",
+                                city: "denver",state: "CO",zip: "80206",email: "merchant@gmail.com",password: "hamburger2", role: 3)
+      visit '/'
+      click_link 'Login'
+      expect(current_path).to eq('/login')
 
-      visit "merchants/#{bike_shop.id}"
+      fill_in :email, with: admin_user.email
+      fill_in :password, with: 'hamburger2'
+      click_button 'Log In'
+
+      visit "/merchants/#{bike_shop.id}"
 
       click_on "Delete Merchant"
 
