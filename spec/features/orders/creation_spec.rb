e909cc350fb93ca5d82c80f9e5813c178efe5e6e
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-27 14:15:04 -0700

    Add test an order cant be created if some of the items are inactive

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -20,9 +20,10 @@ RSpec.describe("Order Creation") do
                         role: 0)
 
       visit '/login'
-        fill_in :email, with: @user.email
-        fill_in :password, with: "password1"
+      fill_in :email, with: @user.email
+      fill_in :password, with: "password1"
       click_on "Log In"
+
       visit "/items/#{@paper.id}"
       click_on "Add To Cart"
       visit "/items/#{@paper.id}"
@@ -37,8 +38,6 @@ RSpec.describe("Order Creation") do
     end
 
     it 'I can create a new order' do
-
-
       fill_in :name, with: @user.name
       fill_in :address, with: @user.street_address
       fill_in :city, with: @user.city
@@ -113,6 +112,42 @@ RSpec.describe("Order Creation") do
 
       expect(page).to have_content("Please complete address form to create an order.")
       expect(page).to have_button("Create Order")
+    end
+  end
+
+  describe "inactive items in order" do
+    before :each do
+      @mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @user = User.create(name: "Bert",
+                        street_address: "123 Sesame St.",
+                        city: "NYC",
+                        state: "New York",
+                        zip: 10001,
+                        email: "bert2@gmail.com",
+                        password: "password1",
+                        password_confirmation: "password1",
+                        role: 0)
+
+      visit '/login'
+      fill_in :email, with: @user.email
+      fill_in :password, with: "password1"
+      click_on "Log In"
+    end
+
+    it "cant create order if an item is inactive" do
+      @tire.update(active?: false)
+
+      visit "/items/#{@pencil.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@tire.id}"
+      click_on "Add To Cart"
+
+      expect(current_path).to eq("/items/#{@tire.id}")
+      expect(page).to have_content("This item is inactive")
     end
   end
 end
