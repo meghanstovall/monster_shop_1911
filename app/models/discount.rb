126d397a680904ddcf0238ffed22ef9f05439c7a
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-04 10:19:06 -0700

    Syntax

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,6 @@
 class Discount < ApplicationRecord
   validates_presence_of :name, :percent_off, :min_quantity
+  validates_presence_of :percent_off, less_than_or_equal_to: 100
   belongs_to :merchant
   has_many :item_discounts, dependent: :destroy
   has_many :items, through: :item_discounts
