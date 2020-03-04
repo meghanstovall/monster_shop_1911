37d7638719f013ab9dd44d09cc584b7316c28b74
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-04 12:51:47 -0700

    Fix discount validates presence

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,8 @@
 class Discount < ApplicationRecord
-  validates_presence_of :name, :percent_off, :min_quantity
-  validates_presence_of :percent_off, less_than_or_equal_to: 100
+  validates :name, presence: true
+  validates :percent_off, presence: true, numericality: { less_than_or_equal_to: 100 }
+  validates :min_quantity, presence: true, numericality: { more_than: 0 }
+
   belongs_to :merchant
   has_many :item_discounts, dependent: :destroy
   has_many :items, through: :item_discounts
